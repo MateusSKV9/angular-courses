2899375882d59d728ea1241e5d3fd6a63ad65947
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-06 21:35:13 -0300

    routes

--- curso-cafe-com-bug/src/app/app.component.ts
+++ curso-cafe-com-bug/src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { BaseUiComponent } from "./components/base-ui/base-ui.component";
-import { UsersComponent } from "./components/users/users.component";
+import { BaseUiComponent } from './components/base-ui/base-ui.component';
+import { UsersComponent } from './components/users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, NavbarComponent, FooterComponent, CommonModule, BaseUiComponent, UsersComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    CommonModule,
+    BaseUiComponent,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'curso-cafe-com-bug';
-  exibeHome: boolean  = true;
+  exibeHome: boolean = true;
 
   destruir() {
     this.exibeHome = false;
