8986285345d63acfb94221317a793f808fdb25e0
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-09 16:28:35 -0300

    displaying messages with npx-toastr

--- curso-cafe-com-bug/src/app/pages/home/home.component.ts
+++ curso-cafe-com-bug/src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { UserService } from '../../services/user.service';
 import { UsersComponent } from '../../components/users/users.component';
 import { UserGit } from '../../interfaces/UserGit';
 import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -17,17 +18,25 @@ export class HomeComponent {
   userGit!: UserGit | undefined;
   username: string = '';
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+  ) {}
 
   getGitUser() {
     this.userService.getGitUser(this.username).subscribe({
       next: (response: UserGit) => {
+        this.toastr.success('Usuário encontrado', 'Sucesso', {
+          timeOut: 1500,
+        });
         this.userGit = response;
       },
       error: (error) => {
         this.userGit = undefined;
-        alert(error.error.message)
-      }
+        this.toastr.error(error.error.message, 'Erro', {
+          timeOut: 1500,
+        });
+      },
     });
   }
 
