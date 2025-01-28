91e029f1d490c86633e839b5b05b9b97cc08cd84
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-28 18:45:03 -0300

    feat: created product service

---
+++ curso-code-dimension/src/app/services/products.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Product } from '../interfaces/Product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  baseUrl = environment.baseUrl;
+  apiUrl = `${this.baseUrl}/products`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+}
