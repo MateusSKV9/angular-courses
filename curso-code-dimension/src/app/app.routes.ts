abb1e23e38d53abb4d3abbd4ccf68f7be820b4bd
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-30 12:18:42 -0300

    feat(routes): set create-product route with lazy-loading

--- curso-code-dimension/src/app/app.routes.ts
+++ curso-code-dimension/src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
-export const routes: Routes = [{ path: '', component: ListComponent }];
+export const routes: Routes = [
+  { path: '', component: ListComponent },
+  {
+    path: 'create-product',
+    loadComponent: () =>
+      import('./components/create-product/create-product.component').then(
+        (m) => m.CreateProductComponent
+      ),
+  },
+];
