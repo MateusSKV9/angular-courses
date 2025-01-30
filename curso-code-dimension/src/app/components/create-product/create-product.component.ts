2a44b870cfc21d270e40bd72198d45e6e7b55bc2
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-30 12:20:25 -0300

    feat(create-product): add create-product component

---
+++ curso-code-dimension/src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-product',
+  imports: [],
+  standalone: true,
+  templateUrl: './create-product.component.html',
+  styleUrl: './create-product.component.css',
+})
+export class CreateProductComponent {}
