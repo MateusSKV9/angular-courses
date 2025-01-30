cbb6c97b770ad48516836ec5abb653aafb62c674
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-30 19:27:50 -0300

    refactor(list): add dynamic listing with signals

--- curso-code-dimension/src/app/components/list/list.component.ts
+++ curso-code-dimension/src/app/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/Product';
 import { CardComponent } from '../card/card.component';
@@ -13,13 +13,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './list.component.css',
 })
 export class ListComponent implements OnInit {
-  products: Product[] = [];
+  products = signal<Product[]>([]);
 
   constructor(private productsservice: ProductsService) {}
 
   ngOnInit(): void {
     this.productsservice.getAllProducts().subscribe((products) => {
-      this.products = products;
+      this.products.set(products);
     });
+  }
+
+  onProductDeleted(productId: string) {
+    this.products.update((products) =>
+      products.filter((p) => p.id !== productId)
+    );
   }
 }
