164669703324e12b70d61a2e075127750031634e
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-01 14:17:46 -0300

    feat: add back-to-list component

---
+++ curso-code-dimension/src/app/shared/back-to-list/back-to-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-back-to-list',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './back-to-list.component.html',
+  styleUrl: './back-to-list.component.css',
+})
+export class BackToListComponent {}
