4b9e35c9b5a3691edf0ddb311eb05c5eeb896303
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-05 21:09:55 -0300

    feat: add mock heroes

---
+++ curso-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './interfaces/Hero.interface';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Flash' },
+  { id: 3, name: 'Superman' },
+  { id: 4, name: 'Aquaman' },
+];
