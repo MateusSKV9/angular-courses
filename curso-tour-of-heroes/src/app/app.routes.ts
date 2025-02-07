0175b75eb942413a3bbfc8444795f8b453e6455e
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-07 15:18:25 -0300

    feat: implement activatedRoute

--- curso-tour-of-heroes/src/app/app.routes.ts
+++ curso-tour-of-heroes/src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDatailComponent } from './components/hero-datail/hero-datail.component';
 
-export const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes/:id', component: HeroDatailComponent },
+  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
+];
--- curso-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ curso-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -2,27 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/Hero.interface';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from '../../mock-heroes';
-import { HeroDatailComponent } from '../hero-datail/hero-datail.component';
 import { HeroService } from '../../services/hero.service';
-import { MessageService } from '../../services/message.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeroDatailComponent],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
   host: { class: 'class1' },
 })
 export class HeroesComponent implements OnInit {
   heroes?: Hero[];
-  selectedHero!: Hero;
 
-  constructor(
-    private heroService: HeroService,
-    private messaseService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -30,12 +24,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  onSelectHero(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messaseService.addMessage(
-      `HeroesComponent: Selected hero: ${hero.id} - ${hero.name}`
-    );
   }
 }
